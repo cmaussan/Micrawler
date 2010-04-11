8daa00a7bc65fdb325d190a9e485867c47c38c86
cmaussan <cmaussan@linkfluence.net>
2010-04-11 17:26:07 +0200

    initial commit

---
+++ lib/Micrawler.pm
@@ -0,0 +1,30 @@
+package Micrawler;
+
+our $VERSION = '0.0.01';
+
+use Moose;
+
+1;
+__END__
+
+=head1 NAME
+
+Micrawler -
+
+=head1 SYNOPSIS
+
+  use Micrawler;
+
+=head1 DESCRIPTION
+
+Micrawler is
+
+=head1 AUTHOR
+
+Camille Maussang E<lt>camille.maussang {at} linkfluence.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (c) 2010 RTGI
